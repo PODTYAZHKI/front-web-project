e33097eee29cecff41b228a3a697ed9536348baf
Renamethis <ops282@mail.ru>
2022-03-17 15:49:34 +0300

    Implemented tfjs pages

---
+++ src/views/ToxicityPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-btn @click="toxicity()">Пример toxicity</v-btn>
+</template>
+
+<script>
+export default {
+  methods: {
+    toxicity() {
+      const toxicity = require('@tensorflow-models/toxicity');
+      const threshold = 0.9;
+      toxicity.load(threshold).then(model => {
+        const sentences = ['you suck'];
+        model.classify(sentences).then(predictions => { console.log(predictions);});
+      });
+    }
+  }
+};
+</script>
+
+<style></style>
---
+++ src/views/UsePage.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-btn @click="use()">Пример Universal Sentence Encoder</v-btn>
+</template>
+
+<script>
+export default {
+  methods: {
+    use() {
+      const use = require('@tensorflow-models/universal-sentence-encoder');
+      use.load().then(model => {
+        const sentences = [
+             'Hello.',
+            'How are you?'
+        ];
+        model.embed(sentences).then(embeddings => {
+            embeddings.print(true);
+        });
+    });
+    }
+  }
+};
+</script>
+
+<style></style>
