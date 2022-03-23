98e6023b40db7838f3aa067237c05b261838fc6d
Renamethis <ops282@mail.ru>
2022-03-23 04:40:39 +0300

    Django translate endpoint

--- django/APIViewset.py
+++ django/APIViewset.py
@@ -4,6 +4,7 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 import json
 import requests
+import subprocess
 
 class API(ViewSet):
     http_method_names = ['post']
@@ -11,28 +12,30 @@ class API(ViewSet):
     api_url = "http://translate.api.cloud.yandex.net/translate/v2/translate/"
     folder_id = "b1goiou8tkto0joul46k"
     def create(self, request, *args, **kwargs):
-        try:
-            body = json.loads(request.body)
-            text = body["text"]
-            language = body["targetLanguage"]
-            with open("key.json", "r") as file:
-                token = file.read().replace('\n', '')
-                headers = {
-                    'Content-Type': 'application/json',
-                    "Authorization": "Bearer {0}".format(token)
-                }
-                data = {
-                    "folderId": self.folder_id,
-                    "texts": [text],
-                    "targetLanguageCode": language
-                }
-                response = requests.post('https://translate.api.cloud.yandex.net/translate/v2/translate',
-                    json = data,
-                    headers = headers
-                )
-            return Response({
-                "status": "ok",
-                "text": response.json()["translations"][0]["text"]
-            }, status=status.HTTP_200_OK)
-        except:
-            return Response({"status":"error"}, status=status.HTTP_400_BAD_REQUEST)
+        #try:
+        with open("key.json", "w") as file:
+            subprocess.run(["yc", "iam", "create-token"], stdout=file)
+        body = json.loads(request.body)
+        text = body["text"]
+        language = body["targetLanguage"]
+        with open("key.json", "r") as file:
+            token = file.read().replace('\n', '')
+            headers = {
+                'Content-Type': 'application/json',
+                "Authorization": "Bearer {0}".format(token)
+            }
+            data = {
+                "folderId": self.folder_id,
+                "texts": [text],
+                "targetLanguageCode": language
+            }
+            response = requests.post('https://translate.api.cloud.yandex.net/translate/v2/translate',
+                json = data,
+                headers = headers
+            )
+        return Response({
+            "status": "ok",
+            "text": response.json()["translations"][0]["text"]
+        }, status=status.HTTP_200_OK)
+        #except:
+        #    return Response({"status":"error"}, status=status.HTTP_400_BAD_REQUEST)
